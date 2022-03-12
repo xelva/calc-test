4d98d72171d854dd4753a41f1e67322caa28b913
xelva <xelvacodes@gmail.com>
2022-03-12 17:47:12 -0600

    calculator works

--- js/script-backup.js
+++ js/script-backup.js
@@ -1,9 +1,15 @@
+//store array of all elements w class of 'numbers' in new variable
 const numbers = document.querySelectorAll('.numbers div');
+//store array of all elements w class of 'calcButton' in new variable
 const operations = document.querySelectorAll('.calcButton');
+//variable for calling the equals button
+const calcResult = document.getElementById('result');
 //set the decimal flag to false, meaning the decimal has not been entered yet
 let decimalFlag = false;
 //set the operator to false, meaning an operator has not been entered yet
 let operatorFlag = false;
+//create an equals flag
+let equalFlag = false;
 //variable for the input field
 const input = document.getElementById('input');
 //variable to hold a string of numbers
@@ -12,35 +18,33 @@ let holdNums = '';
 let numArray = [];
 //array to store operators when cleared
 let opsArray = [];
+//result array
+let resultArray = [];
 
 //loop through the array of elements with class of 'numbers' and add addEventListener to each
 for (let i = 0; i<numbers.length; i++) {
     numbers[i].addEventListener('click', function(e) {
         //clear the input, arrays, and flags if 'C' is clicked
         if (e.target.innerHTML === 'C') {
-            input.innerHTML = '';
-            decimalFlag = false;
-            operatorFlag = false;
-            opsArray = [];
-            numArray = [];
+            clearC();
         }
         //if 'C' is not clicked, do the following
         else {
+            //clear input if results are currently displayed 
+            if (!equalFlag) {
+                input.innerHTML = '';
+                equalFlag = true;
+            }
             //if an operator has been entered before your number
             if (operatorFlag) {
-                //add the operator to an array to store it
-                opsArray.push(input.innerHTML);
-                //...and print to console :)
-                console.log(opsArray);
-                //clear the input
-                input.innerHTML = '';
-                //reset the operatorFlag so that an operator can be entered after numbers
-                operatorFlag = false;
-                //reset the decimalFlag
-                decimalFlag = false;
+                //add the operator to an array and clear the input
+               opReset();
             }
             //if the user is not entering a decimal, add the number to input
             if(e.target.innerHTML != ".") {
+                //but if you previously were displaying a result, clear the input first
+                
+                //then add your number
                 input.innerHTML += e.target.innerHTML; 
             }
             //else if the user clicks a decimal for the first time (we know this bc flag is negative), add decimal and set the flag to positive
@@ -67,13 +71,110 @@ for (let i = 0; i<operations.length; i++) {
             input.innerHTML = '';
             //add the operator
             input.innerHTML += e.target.innerHTML; 
-            //console.log(holdNums);
+            
             }
-        //if an operator has been clicked, don't do anything (except add a small message to user)
+        //if an operator has been clicked, don't do anything (except add a message to user)
         else if (operatorFlag) {
             alert('Please don\'t click that...you\'ve already entered an operator.')
             
         }
     })
 };
+
+calcResult.addEventListener('click', function(){
+    //set the flag to true
+    operatorFlag = true;
+    //add the numbers that are currently in the display to an array before clearing them
+    numArray.push(input.innerHTML);
+    //show that it's working in console
+    //console.log(numArray);
+    //clear the input display
+    input.innerHTML = '';
+    console.log(numArray);
+    for (let i = 0; i < numArray.length; i++) {
+
+            console.log(numArray[i]);
+
+            if (resultArray.length === 0){
+                resultArray.push(numArray[i]);
+                console.log('holdnum for the first time');
+               
+            }
+            else {
+                if (opsArray[0] === '+') {
+                    opsArray.shift();
+                    holdRes = Number(resultArray[0]) + Number(numArray[i]);
+                    resultArray[0] = holdRes;
+                    console.log('done with this round');
+                    
+                    
+                }
+                else if (opsArray[0] === '-') {
+                    opsArray.shift();
+                    holdRes = Number(resultArray[0]) - Number(numArray[i]);
+                    resultArray[0] = holdRes;
+                    console.log('done with this round');
+                    
+                    
+                }
+                else if (opsArray[0] === '*') {
+                    opsArray.shift();
+                    holdRes = Number(resultArray[0]) * Number(numArray[i]);
+                    resultArray[0] = holdRes;
+                    console.log('done with this round');
+                   
+                    
+                }
+                else {
+                    opsArray.shift();
+                    holdRes = Number(resultArray[0]) / Number(numArray[i]);
+                    resultArray[0] = holdRes;
+                    console.log('done with this round');
+                    
+                }
+            }
+    }
+    input.innerHTML = resultArray;
+    decimalFlag = false;
+    operatorFlag = false;
+    opsArray = [];
+    numArray = [];
+    resultArray = [];
+    equalFlag = false;
+
+});
+
+//when the equals button is clicked
+    //pop the last remaining number in array
+    //convert string to numbers and stor ein new array (map)
+    //loop through each array item of numbers 
+    //get first item in ops array, perform the first action in the operator array
+        //maybe something like if statement with four operator options 
+    //use something like shift splice to remove the first item after the loop
+        //maybe opsArray.splice(0,1) or myOps.shift()
+
+
+//what happens when an operator is clicked and then a number
+const opReset = () => {
+    //add the operator to an array to store it
+    opsArray.push(input.innerHTML);
+    //clear the input
+    input.innerHTML = '';
+    //reset the operatorFlag so that an operator can be entered after numbers
+    operatorFlag = false;
+    //reset the decimalFlag
+    decimalFlag = false;
+};
+
+//what happens when you click clear
+const clearC = () => {
+    input.innerHTML = '';
+    decimalFlag = false;
+    operatorFlag = false;
+    opsArray = [];
+    numArray = [];
+    resultArray = [];
+};
+
+
 
